39a3ca7fd9e43d7037d37777c5c6bf3d50a76c66
Pa0608 <e247448@edu.sbl.ch>
2023-03-10 09:30:24 +0100

    movement

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
-kaboom();
+//kaboom();
 
-loadSprite("bean", "assets/bean.png");
+
+loadSprite("pilot", "assets/pilot.jpg")
+
+// Define player movement speed (pixels per second)
+const SPEED = 320
+
+// Add player game object
+const player = add([
+	sprite("pilot"),
+	// center() returns the center point vec2(width() / 2, height() / 2)
+	pos(center()),
+  scale(0.15), // Set the initial scale of the player to 50%
+  
+  
+])
+
+// onKeyDown() registers an event that runs every frame as long as user is holding a certain key
+onKeyDown("left", () => {
+	// .move() is provided by pos() component, move by pixels per second
+	player.move(-SPEED, 0)
+})
+
+onKeyDown("right", () => {
+	player.move(SPEED, 0)
+})
+
+onKeyDown("up", () => {
+	player.move(0, -SPEED)
+})
+
+onKeyDown("down", () => {
+	player.move(0, SPEED)
+})
+
+// onClick() registers an event that runs once when left mouse is clicked
+onClick(() => {
+	// .moveTo() is provided by pos() component, changes the position
+	player.moveTo(mousePos())
+})
 
 add([
-  sprite("bean"),
-  pos(80,40)
-]);
+	// text() component is similar to sprite() but renders text
+	text("Press arrow keys", { width: width() / 2 }),
+	pos(12, 12),
+])
